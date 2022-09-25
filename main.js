674b49ee9ce677b85c15ea1621d739da5e093e78
Caroline Thostrup <caro342h@stud.kea.dk>
2022-09-25 20:12:39 +0200

    fetch data js function structure

--- main.js
+++ main.js
@@ -1,9 +1,51 @@
 "use strict";
 
-/* connectivity test */
 window.addEventListener("DOMContentLoaded", start);
 
-/* function start() {
-  alert("Hello World");
+// Student JSON Data
+const jsonURL = "https://petlatkea.dk/2021/hogwarts/students.json";
+
+// Global Student Array
+let allStudents = [];
+
+let studentList;
+
+// Student Prototype Object
+const Student = {
+  firstName: "-unknown-",
+  lastName: "-unknown-",
+  middleName: "-unknown-",
+  nickName: "-unknown-",
+  gender: "",
+  image: ".png",
+  house: "",
+  isPrefect: 0,
+  isInqSquad: 0,
+};
+
+function start() {
+  console.log("Ready to start!");
+  loadJSON();
 }
- */
+async function loadJSON() {
+  const response = await fetch(jsonURL);
+  const jsonData = await response.json();
+
+  // when loaded, prepare data objects
+  prepareObjects(jsonData);
+}
+
+function prepareObjects(jsonData) {
+  allStudents = jsonData.map(prepareObject);
+  displayList(allStudents);
+}
+
+function prepareObject(jsonObject) {
+  return student;
+}
+
+function displayList(students) {
+  allStudents.forEach(displayStudent);
+}
+
+function displayStudent(student) {}
